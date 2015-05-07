b28b131e22215aefcbbbf51d295af4cb4e99471b
LauraKirby <laurakirby26@gmail.com>
2015-05-07 11:26:46 -0700

    add boiler plate code for solving average function and set up jasmine

---
+++ spec/spec.js
@@ -0,0 +1,11 @@
+//Write a function letterGrader which takes a test score and 
+//returns the equivalent letter grade.
+
+var letterGrader = require("../grading_exercise.js")
+
+describe('the letterGrader function',
+	it ('takes a test score and returns the equivalent letter grade', function (){
+	  expect(letterGrader.average(num, ())).toEqual();
+	}); 
+});
+
