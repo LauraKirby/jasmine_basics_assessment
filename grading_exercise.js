7024ec9f37f154e99619ebec84742d8be4b5ba6c
LauraKirby <laurakirby26@gmail.com>
2015-05-07 11:47:03 -0700

    add function and test for average test score

--- grading_exercise.js
+++ grading_exercise.js
@@ -1,5 +1,9 @@
 module.exports = { 
-	average: function(){
-
+	average: function(arr){
+		sum = 0; 
+		for (var i = 0; i<arr.length; i++) {
+			sum += parseInt(arr[i], 10); 
+		}
+		return sum/arr.length; 
 		}
 }
--- spec/spec.js
+++ spec/spec.js
@@ -1,11 +1,20 @@
 //Write a function letterGrader which takes a test score and 
 //returns the equivalent letter grade.
 
-var letterGrader = require("../grading_exercise.js")
+var grader = require("../grading_exercise.js")
 
-describe('the letterGrader function',
-	it ('takes a test score and returns the equivalent letter grade', function (){
-	  expect(letterGrader.average(num, ())).toEqual();
+describe('the test score average function', function() {
+	it ('should take test scores and return the average', function (){
+	  expect(grader.average([90, 95, 87, 60])).toEqual(83);
 	}); 
 });
+
+//  will do this next, started on the average first by mistake 
+//letterGrader
+//describe('the letterGrader function',
+// 	it ('takes a test score and returns the equivalent letter grade', function (){
+// 	  expect(letterGrader.average(90).toEqual();
+// 	}); 
+// });
+
 
